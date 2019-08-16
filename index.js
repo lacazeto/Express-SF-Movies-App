66b1f49afd474a168d018f09fb89b8c672f63c0a
lacazeto <lucas.cazeto@gmail.com>
2019-08-16 22:15:08 +0200

    created basic 1 route static html app

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
+
+// specifies the root directory from which to serve static assets
+app.use(express.static('views'));
+
+app.get('/', (_req, res) => {
+  res.sendFile('./index.html');
+});
+
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Server listening on port ${port}`);
+});
