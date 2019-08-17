d2ef60d84d12b2a73d35e7460e5f87539fd67658
lacazeto <lucas.cazeto@gmail.com>
2019-08-17 20:36:36 +0200

    moved csv parser to backend as api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import MoviesCsvParser from './helpers/movies_csv_parser';
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,12 @@ app.use(express.static('static'));
 
 app.get('/', (_req, res) => {
   res.sendFile('./index.html');
+});
+
+app.get('/movies', (_req, res) => {
+  const moviesCsvParser = new MoviesCsvParser();
+  const { movies } = moviesCsvParser;
+  res.json(movies);
 });
 
 app.listen(port, () => {
