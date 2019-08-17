4b5c7562dfebf7215480a59f60736be2f0c3b925
lacazeto <lucas.cazeto@gmail.com>
2019-08-17 21:05:28 +0200

    receiving data on the frontend

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,38 @@
-import * as $ from 'jquery';
-import MoviesCsvParser from './movies_csv_parser';
+let movies;
 
-const moviesTitles = [];
+const getMovies = (() => {
+  $.ajax({
+    url: '/movies',
+  }).done((data) => {
+    movies = data;
+    console.log(movies);
+  });
+});
+
+const substringMatcher = (strs) => function findMatches(q, cb) {
+  const matches = [];
+  const substrRegex = new RegExp(q, 'i');
+  $.each(strs, (i, str) => {
+    if (substrRegex.test(str)) {
+      matches.push(str);
+    }
+  });
+
+  cb(matches);
+};
+
+getMovies();
 
 $(() => {
-  const moviesCsvParser = new MoviesCsvParser();
-  const { movies } = moviesCsvParser;
   console.log(movies);
+  // Using typeahead, more can be found here: https://twitter.github.io/typeahead.js/examples/
+  $('#typeahead-input .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1,
+  },
+  {
+    name: 'states',
+    source: substringMatcher(movies),
+  });
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,17 +1,40 @@
 "use strict";
 
-var $ = _interopRequireWildcard(require("jquery"));
+var movies;
 
-var _movies_csv_parser = _interopRequireDefault(require("./movies_csv_parser"));
+var getMovies = function getMovies() {
+  $.ajax({
+    url: '/movies'
+  }).done(function (data) {
+    movies = data;
+    console.log(movies);
+  });
+};
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+var substringMatcher = function substringMatcher(strs) {
+  return function findMatches(q, cb) {
+    var matches = [];
+    var substrRegex = new RegExp(q, 'i');
+    $.each(strs, function (i, str) {
+      if (substrRegex.test(str)) {
+        matches.push(str);
+      }
+    });
+    cb(matches);
+  };
+};
 
-function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj["default"] = obj; return newObj; } }
-
-var moviesTitles = [];
+getMovies();
 $(function () {
-  var moviesCsvParser = new _movies_csv_parser["default"]();
-  var movies = moviesCsvParser.movies;
-  console.log(movies);
+  console.log(movies); // Using typeahead, more can be found here: https://twitter.github.io/typeahead.js/examples/
+
+  $('#typeahead-input .typeahead').typeahead({
+    hint: true,
+    highlight: true,
+    minLength: 1
+  }, {
+    name: 'states',
+    source: substringMatcher(movies)
+  });
 });
 //# sourceMappingURL=main.js.map
